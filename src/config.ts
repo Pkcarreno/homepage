6bb56ed9dbc098d0dc781edea30301ef854a2ed3
Pedro Carreño <pkcarrenodev@gmail.com>
2023-01-15 17:21:11 -0400

    feat(css): add font and info

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Pedro Carreno'
+export const SITE_DESCRIPTION = 'Personal Website'
+export const HOMEPAGE_URL = 'https://www.pkcarreno.com/'
