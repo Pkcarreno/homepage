399408441e4316e4c5e90717ebfc2995d7a6d036
Pedro Carreño <pkcarrenodev@gmail.com>
2023-01-15 17:27:18 -0400

    fix(sintaxys): missing comma

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.pkcarreno.com'
+  site: 'https://www.pkcarreno.com',
   integrations: [react(), tailwind()]
 });
