b4ec263bb202afd64721e2ba47da0116e3b1222b
Pedro Carreño <pkcarrenodev@gmail.com>
2023-01-15 18:59:00 -0400

    feat: add navbar and toggling theme

---
+++ src/components/ThemeToggleButton.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react'
+import { IoSunny, IoMoon } from 'react-icons/io5/index.js'
+
+const themes = ['light', 'dark']
+
+export default function ThemeToggler() {
+  const [isMounted, setIsMounted] = useState(false)
+  const [theme, setTheme] = useState(() => {
+    if (import.meta.env.SSR) {
+      return undefined
+    }
+    if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
+      return localStorage.getItem('theme')
+    }
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      return 'dark'
+    }
+    return 'light'
+  })
+  const toggleTheme = () => {
+    const t = theme === 'light' ? 'dark' : 'light'
+    localStorage.setItem('theme', t)
+    setTheme(t)
+  }
+
+  useEffect(() => {
+    const root = document.documentElement
+    if (theme === 'light') {
+      root.classList.remove('dark')
+    } else {
+      root.classList.add('dark')
+    }
+  }, [theme])
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  return isMounted ? (
+    <div className='inline-flex items-center p-[1px]'>
+      <button 
+        className={`bg-purple-800 text-[#f0f0f0] dark:bg-amber-300 dark:text-[#1c1917] cursor-pointer rounded-2xl p-2`}
+        onClick={toggleTheme}
+        arial-label="Toggle theme"
+      >
+        {theme === 'light' ? <IoMoon /> : <IoSunny />}
+      </button>
+    </div>
+  ) : (
+    <div />
+  )
+}
