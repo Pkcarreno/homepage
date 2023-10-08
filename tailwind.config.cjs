06f34bd0b8183a2975b96e689c96345e3cc65d76
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2023-10-08 13:54:12 -0400

    feat(styles): new font and font styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,10 +3,24 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
 	theme: {
+    fontSize: {
+      sm: '0.833rem',
+      base: '1rem',
+      xl: '1.200rem',
+      '2xl': '1.440rem',
+      '3xl': '1.728rem',
+      '4xl': '2.074rem',
+      '5xl': '2.489rem',
+    },    
+    fontFamily: {
+      heading: 'Poppins',
+      body: 'Poppins',
+    },
+    fontWeight: {
+      normal: '400',
+      bold: '700',
+    },
 		extend: {
-      fontFamily: {
-        notoSans: ["'Noto Sans'", 'Verdana', 'sans-serif']
-      },
       colors: {
        'text': 'hsl(var(--text))',
        'background': 'hsl(var(--background))',
