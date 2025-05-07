2408c49c3d7e6e159d839ea47f42a08c9c946d08
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2025-05-07 18:35:32 -0400

    feat: migrate to biome

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,9 +4,7 @@ export default {
   arrowParens: 'avoid',
   singleQuote: true,
   bracketSpacing: true,
-  endOfLine: 'lf',
   semi: false,
-  tabWidth: 2,
   trailingComma: 'none',
   overrides: [
     {
--- astro.config.js
+++ astro.config.js
@@ -5,84 +5,75 @@ import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel'
 import keystatic from '@keystatic/astro'
 import umami from '@yeskunall/astro-umami'
-import { defineConfig, envField } from 'astro/config'
 import favicons from 'astro-favicons'
 import robots from 'astro-robots'
-import Oxlint from 'unplugin-oxlint/vite'
+import { defineConfig, envField } from 'astro/config'
 
 import { config } from './src/config'
 
 // https://astro.build/config
 export default defineConfig({
-  build: {
-    inlineStylesheets: 'always'
-  },
-  server: {
-    port: 4200
-  },
-  compressHTML: true,
-  prefetch: true,
+	build: {
+		inlineStylesheets: 'always'
+	},
+	server: {
+		port: 4200
+	},
+	compressHTML: true,
+	prefetch: true,
 
-  devToolbar: {
-    enabled: false
-  },
-  env: {
-    schema: {
-      UMAMI_ID: envField.string({
-        access: 'public',
-        context: 'client',
-        optional: false
-      }),
-      UMAMI_ENDPOINT: envField.string({
-        access: 'public',
-        context: 'client',
-        optional: false
-      })
-    }
-  },
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-      nesting: true
-    }),
-    favicons(),
-    robots({
-      policy: [
-        {
-          userAgent: config.iaBotAgents,
-          disallow: ['/']
-        }
-      ]
-    }),
-    sitemap(),
-    markdoc(),
-    react(),
-    keystatic(),
-    !!process.env.UMAMI_ID &&
-      umami({
-        id: process.env.UMAMI_ID,
-        domains: ['pkcarreno.com', 'www.pkcarreno.com'],
-        endpointUrl: process.env.UMAMI_ENDPOINT,
-        hostUrl: process.env.UMAMI_ENDPOINT
-      })
-  ],
+	devToolbar: {
+		enabled: false
+	},
+	env: {
+		schema: {
+			UMAMI_ID: envField.string({
+				access: 'public',
+				context: 'client',
+				optional: false
+			}),
+			UMAMI_ENDPOINT: envField.string({
+				access: 'public',
+				context: 'client',
+				optional: false
+			})
+		}
+	},
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+			nesting: true
+		}),
+		favicons(),
+		robots({
+			policy: [
+				{
+					userAgent: config.iaBotAgents,
+					disallow: ['/']
+				}
+			]
+		}),
+		sitemap(),
+		markdoc(),
+		react(),
+		keystatic(),
+		!!process.env.UMAMI_ID &&
+			umami({
+				id: process.env.UMAMI_ID,
+				domains: ['pkcarreno.com', 'www.pkcarreno.com'],
+				endpointUrl: process.env.UMAMI_ENDPOINT,
+				hostUrl: process.env.UMAMI_ENDPOINT
+			})
+	],
 
-  output: 'static',
+	output: 'static',
 
-  vite: {
-    optimizeDeps: {
-      exclude: ['@resvg/resvg-js']
-    },
-    plugins: [
-      Oxlint({
-        watch: true,
-        fix: true,
-        includes: ['src/**/*.{ts,astro}'],
-        packageManager: 'pnpm'
-      })
-    ]
-  },
+	vite: {
+		optimizeDeps: {
+			exclude: ['@resvg/resvg-js']
+		}
+	},
 
-  site: config.site,
-  adapter: vercel()
+	site: config.site,
+	adapter: vercel()
 })
