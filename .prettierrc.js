72f6637faeb2976e39c1598a3a9a32f4860d228f
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2025-05-07 18:47:25 -0400

    feat: add prettier tailwind plugin

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ['prettier-plugin-astro'],
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
   arrowParens: 'avoid',
   singleQuote: true,
   bracketSpacing: true,
